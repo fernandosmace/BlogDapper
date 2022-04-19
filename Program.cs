0906fd5f482009c6a4345da6cd85800289d8a901
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-04-19 15:58:55 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=balta;User ID=sa;Password=a2t%JH!a@AgaxAG;trustservercertificate=true";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Name}");
+                }
+            }
+        }
+    }
+}
