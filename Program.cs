c4b3576198a6490a3c09bcbf96d32db686095f5f
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-04-26 15:50:29 -0300

    Add CRUD operations

--- Program.cs
+++ Program.cs
@@ -7,10 +7,15 @@ namespace Blog
 {
     class Program
     {
-        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=balta;User ID=sa;Password=a2t%JH!a@AgaxAG;trustservercertificate=true";
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=a2t%JH!a@AgaxAG;trustservercertificate=true";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //UpdateUser();
+            DeleteUser();
+
         }
 
         public static void ReadUsers()
@@ -23,6 +28,68 @@ namespace Blog
                 {
                     Console.WriteLine($"{user.Name}");
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine($"{user.Name}");
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Equipe Balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe balta.io",
+                PasswordHash="HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+
+                Console.WriteLine($"Cadastro realizado com sucesso!!");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe | Balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe de Suporte balta.io",
+                PasswordHash="HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+
+                Console.WriteLine($"Atualização realizada com sucesso!!");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+
+                Console.WriteLine($"Exclusão realizada com sucesso!!");
             }
         }
     }
