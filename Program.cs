aa6632132b0410f975d81a1242c9cbc12fbf4785
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-04-26 16:56:41 -0300

    Add Repository Pattern

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Blog.Models;
+using Blog.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
@@ -8,89 +9,50 @@ namespace Blog
     class Program
     {
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=a2t%JH!a@AgaxAG;trustservercertificate=true";
+        
         static void Main(string[] args)
         {
-            //ReadUsers();
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            ReadUsers(connection);
             //ReadUser();
             //CreateUser();
             //UpdateUser();
-            DeleteUser();
+            //DeleteUser();
+
+            ReadRoles(connection);
+
+            connection.Close();
 
         }
 
-        public static void ReadUsers()
+
+        public static void ReadUsers(SqlConnection connection)
         {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var users = connection.GetAll<User>();
+            var repository = new UserRepository(connection);
+            var users =  repository.Get();
 
-                foreach (var user in users)
-                {
-                    Console.WriteLine($"{user.Name}");
-                }
-            }
-        }
-
-        public static void ReadUser()
-        {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(1);
-
+            foreach (var user in users)
                 Console.WriteLine($"{user.Name}");
-            }
         }
 
-        public static void CreateUser()
+        public static void ReadUser(SqlConnection connection)
         {
-            var user = new User()
-            {
-                Bio = "Equipe Balta.io",
-                Email = "hello@balta.io",
-                Image = "https://...",
-                Name = "Equipe balta.io",
-                PasswordHash="HASH",
-                Slug = "equipe-balta"
-            };
-
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Insert<User>(user);
-
-                Console.WriteLine($"Cadastro realizado com sucesso!!");
-            }
+            var repository = new UserRepository(connection);
+            var user = repository.Get(2);
+            
+            Console.WriteLine($"{user.Name}");
         }
 
-        public static void UpdateUser()
+        public static void ReadRoles(SqlConnection connection)
         {
-            var user = new User()
-            {
-                Id = 2,
-                Bio = "Equipe | Balta.io",
-                Email = "hello@balta.io",
-                Image = "https://...",
-                Name = "Equipe de Suporte balta.io",
-                PasswordHash="HASH",
-                Slug = "equipe-balta"
-            };
+            var repository = new RoleRepository(connection);
+            var roles =  repository.Get();
 
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Update<User>(user);
-
-                Console.WriteLine($"Atualização realizada com sucesso!!");
-            }
+            foreach (var role in roles)
+                Console.WriteLine($"{role.Name}");
         }
-
-        public static void DeleteUser()
-        {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(2);
-                connection.Delete<User>(user);
-
-                Console.WriteLine($"Exclusão realizada com sucesso!!");
-            }
-        }
+        
     }
 }
