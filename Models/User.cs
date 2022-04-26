13a5eb5e9bdd9bced1e44bde07cf0cdc5a0dc959
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-04-26 15:50:02 -0300

    Add DataAnnotation for User table name

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
 
 namespace Blog.Models
 {
+    [Table("[User]")]
     public class User
     {
         public int Id { get; set; }
