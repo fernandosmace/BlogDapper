5e50d2d327b7f4e3c072928b678f5cfc2df28073
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-05-03 11:23:45 -0300

    Otimizando repositorios

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -11,21 +11,40 @@ namespace Blog.Repositories
         private readonly SqlConnection _connection;
 
         public UserRepository(SqlConnection connection)
-            =>  _connection = connection;
+            => _connection = connection;
 
         public IEnumerable<User> Get()
-            =>  _connection.GetAll<User>();
+            => _connection.GetAll<User>();
 
         public User Get(int id)
-            =>  _connection.Get<User>(id);
+            => _connection.Get<User>(id);
 
         public void Create(User user)
-            =>  _connection.Insert<User>(user);
+        {
+            user.Id = 0;
+            _connection.Insert<User>(user);
+        }
 
         public void Update(User user)
-            =>  _connection.Update<User>(user);
+        {
+            if (user.Id != 0)
+                _connection.Update<User>(user);
+        }
 
         public void Delete(User user)
-            =>  _connection.Delete<User>(user);
+        {
+            if (user.Id != 0)
+                _connection.Delete<User>(user);
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var user = _connection.Get<User>(id);
+
+            _connection.Delete<User>(user);
+        }
     }
 }
