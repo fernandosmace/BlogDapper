a42db3c73e44ceca74ec194093d47b132411d075
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-05-27 15:05:46 -0300

    Start menu separated on especific method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Blog.Views;
 using Blog.Views.TagViews;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
@@ -17,59 +18,11 @@ namespace Blog
             Database.Connection = new SqlConnection(CONNECTION_STRING);
             Database.Connection.Open();
 
-            Load();
+            StartView.Load();
 
             Console.ReadKey();
 
             Database.Connection.Close();
         }
-
-
-        private static void Load()
-        {
-            Console.Clear();
-            Console.WriteLine("");
-            Console.WriteLine("O que deseja fazer?");
-            Console.WriteLine();
-            Console.WriteLine("1 - Gestão de Usuários");
-            Console.WriteLine("2 - Gestão de Perfis");
-            Console.WriteLine("3 - Gestão de Categorias");
-            Console.WriteLine("4 - Gestão de Tags");
-            Console.WriteLine("5 - Vincular Usuário à Perfil");
-            Console.WriteLine("6 - Vincular Post à Tag");
-            Console.WriteLine("7 - Relatórios");
-            Console.WriteLine();
-
-            var option = short.Parse(Console.ReadLine()!);
-
-            switch (option)
-            {
-                case 1:
-                    //MenuUserView.Load();
-                    break;
-                case 2:
-                    //MenuRoleView.Load();
-                    break;
-                case 3:
-                    //MenuCategoryView.Load();
-                    break;
-                case 4:
-                    MenuTagView.Load();
-                    break;
-                case 5:
-                    //MenuUserRoleView.Load();
-                    break;
-                case 6:
-                    //MenuPostTagView.Load();
-                    break;
-                case 7:
-                    //MeuReports.Load();
-                    break;
-                default:
-                    Load();
-                    break;
-            }
-        }
-
     }
 }
---
+++ Views/StartView.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Views.TagViews;
+
+namespace Blog.Views
+{
+    public class StartView
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Gestão de Usuários");
+            Console.WriteLine("2 - Gestão de Perfis");
+            Console.WriteLine("3 - Gestão de Categorias");
+            Console.WriteLine("4 - Gestão de Tags");
+            Console.WriteLine("5 - Vincular Usuário à Perfil");
+            Console.WriteLine("6 - Vincular Post à Tag");
+            Console.WriteLine("7 - Relatórios");
+            Console.WriteLine();
+
+            var option = short.Parse(Console.ReadLine()!);
+
+            switch (option)
+            {
+                case 1:
+                    //MenuUserView.Load();
+                    break;
+                case 2:
+                    //MenuRoleView.Load();
+                    break;
+                case 3:
+                    //MenuCategoryView.Load();
+                    break;
+                case 4:
+                    MenuTagView.Load();
+                    break;
+                case 5:
+                    //MenuUserRoleView.Load();
+                    break;
+                case 6:
+                    //MenuPostTagView.Load();
+                    break;
+                case 7:
+                    //MeuReports.Load();
+                    break;
+                default:
+                    Load();
+                    break;
+            }
+        }
+    }
+}
