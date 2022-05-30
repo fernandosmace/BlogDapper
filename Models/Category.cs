b628314c3dde07a667cb418a3f5d55063b09f57e
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2022-05-30 11:16:46 -0300

    Fix Post-Category relation

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,9 +6,11 @@ namespace Blog.Models
     [Table("[Category]")]
     public class Category
     {
-       public int Id { get; set; }
-       public string Name { get; set; }
-       public string Slug { get; set; }
-       public IEnumerable<Post> Posts { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+        [Write(false)]
+        public IList<Post> Posts { get; set; }
     }
 }
